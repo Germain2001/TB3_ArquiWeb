cdeeeedcdc2f3f0587d7fa38d881dd1bd863d72c
SebastianLHF <locofx38@gmail.com>
2022-10-28 14:25:21 -0500

    Agregando dependencia provincia

--- src/main/java/pe/edu/upc/project/andaseguro/repositories/IRouteRepository.java
+++ src/main/java/pe/edu/upc/project/andaseguro/repositories/IRouteRepository.java
@@ -1,4 +1,8 @@
 package pe.edu.upc.project.andaseguro.repositories;
 
-public interface IRouteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.project.andaseguro.entities.Route;
+@Repository
+public interface IRouteRepository extends JpaRepository<Route,Integer> {
 }
